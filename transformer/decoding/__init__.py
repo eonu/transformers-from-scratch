686c1b4cb4b81c9d1a22626d9e62ceba884e1327
Edwin Onuonga <ed@hazy.com>
2024-08-22 11:49:12 +0100

    fix `decoding/__init__.py` imports

--- transformer/decoding/__init__.py
+++ transformer/decoding/__init__.py
@@ -1,5 +1,11 @@
 from transformer.decoding import beam, greedy, nucleus, temperature, top_k
 
+from transformer.decoding.beam import *
+from transformer.decoding.greedy import *
+from transformer.decoding.nucleus import *
+from transformer.decoding.temperature import *
+from transformer.decoding.top_k import *
+
 __all__ = [
     *beam.__all__,
     *greedy.__all__,
